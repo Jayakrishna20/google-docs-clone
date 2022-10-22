a595b86a6c155da352664aa4a907bf2df7c20dd5
Jayakrishnan <jayakrishnans2041@gmail.com>
2022-10-22 19:35:00 +0530

    auth middleware added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const User = require("../models/user")
-
+const jwt = require("jsonwebtoken")
+const auth = require("../middlewares/auth")
 const authRouter = express.Router()
 
 authRouter.post('/api/signup', async (req, res) => {
@@ -18,10 +19,17 @@ authRouter.post('/api/signup', async (req, res) => {
             user = await user.save()
         }
 
-        res.json({ user })
+        const token = jwt.sign({ id: user._id }, "passwordKey")
+
+        res.json({ user, token })
     } catch (e) {
-        res.status(500).json({error:e.message})
+        res.status(500).json({ error: e.message })
     }
+})
+
+authRouter.get('/', auth, async (req, res) => {
+    const user = await user.findById(req.user)
+    res.json({ user, token: req.token })
 })
 
 module.exports = authRouter
