457d57f1df7d01508b43c02a6b473b3f835ccaea
Jayakrishnan <jayakrishnans2041@gmail.com>
2022-10-23 21:07:40 +0530

    added feature: document creation

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_docs_clone/screens/document_screen.dart';
 import 'package:google_docs_clone/screens/home_screen.dart';
 import 'package:google_docs_clone/screens/login_screen.dart';
 import 'package:routemaster/routemaster.dart';
@@ -8,4 +9,9 @@ final loggedOutRoute = RouteMap(routes: {
 });
 final loggedInRoute = RouteMap(routes: {
   '/': (route) => const MaterialPage(child: ScreenHome()),
+  '/document/:id': (route) => MaterialPage(
+        child: ScreenDocument(
+          id: route.pathParameters['id'] ?? '',
+        ),
+      ),
 });
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/colors.dart';
 import 'package:google_docs_clone/repository/auth_repository.dart';
+import 'package:google_docs_clone/repository/document_repository.dart';
+import 'package:routemaster/routemaster.dart';
 
 class ScreenHome extends ConsumerWidget {
   const ScreenHome({super.key});
@@ -9,6 +11,22 @@ class ScreenHome extends ConsumerWidget {
   void signOut(WidgetRef ref) {
     ref.read(authRepositoryProvider).signOut();
     ref.read(userProvider.notifier).update((state) => null);
+  }
+
+  void createDocument(WidgetRef ref, BuildContext context) async {
+    String token = ref.read(userProvider)!.token;
+    final navigator = Routemaster.of(context);
+    final snackBar = ScaffoldMessenger.of(context);
+    final errorModel = await ref.read(documentRepositoryProvider).createDocument(token);
+    if (errorModel.data != null) {
+      navigator.push('/document/${errorModel.data.id}');
+    } else {
+      snackBar.showSnackBar(
+        SnackBar(
+          content: Text(errorModel.error!),
+        ),
+      );
+    }
   }
 
   @override
@@ -19,7 +37,7 @@ class ScreenHome extends ConsumerWidget {
         elevation: 0,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => createDocument(ref, context),
             icon: const Icon(
               Icons.add,
               color: kBlackColor,
